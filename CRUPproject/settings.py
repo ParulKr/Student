9a9a32dd41a15aaa845283aaa5ea62abb5bf8c82
ParulKr <119673996+ParulKr@users.noreply.github.com>
2025-07-08 14:12:43 +0530

    Update settings.py host name

--- CRUPproject/settings.py
+++ CRUPproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3=f58%d(yd+a667!wx%h=)9hi=5_g_i#2s^yn0mz&+be8^(^@a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://blue-wave-0f25c7c00.2.azurestaticapps.net']
 
 
 # Application definition
