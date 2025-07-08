074d9bccb90f11c822b9e7e52591a51763a2410c
Golu <parulkumargolu8986@gmail.com>
2025-07-08 18:21:13 +0530

    add static root file

--- CRUPproject/settings.py
+++ CRUPproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3=f58%d(yd+a667!wx%h=)9hi=5_g_i#2s^yn0mz&+be8^(^@a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['studentwebapp-d6fadha0brekcjau.southindia-01.azurewebsites.net']
+ALLOWED_HOSTS = ['studentwebapp-d6fadha0brekcjau.southindia-01.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
@@ -117,8 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
